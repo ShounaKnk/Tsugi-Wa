ee0068b8b72bd261d4161f3ee450e4c3315f5abd
ShounaKnk <shounakkannaik0607@gmail.com>
2025-03-06 00:33:31 +0530

    saves the similarity matrix separately for faster accsess

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 import difflib
+import os
 
 dataset = pd.read_csv('anime-dataset.csv')
 
@@ -13,9 +14,16 @@ for feature in important_features:
 
 dataset['combined_features'] =dataset["Genres"] + ' ' + dataset["Synopsis"] + ' ' + dataset["Type"] + ' ' +dataset["Aired"] + ' ' + dataset["Studios"] + ' ' + dataset["Source"] + ' ' +dataset["Rating"]
 
-vectorizer = TfidfVectorizer()
-vectorized_features = vectorizer.fit_transform(dataset["combined_features"])
-similarity = cosine_similarity(vectorized_features)
+similarity_matrix = 'cosine_similarity_matrix.npy'
+if os.path.exists(similarity_matrix):
+    similarity = np.load(similarity_matrix)
+    print('loaded saved similarity matrix')
+else:
+    vectorizer = TfidfVectorizer()
+    vectorized_features = vectorizer.fit_transform(dataset["combined_features"])
+    similarity = cosine_similarity(vectorized_features)
+    np.save(similarity_matrix, similarity)
+    print('computed and saved the similarity matrix')
 
 all_anime_jap = dataset['JapaneseName'].tolist()
 all_anime_eng = dataset['EnglishName'].tolist()
@@ -43,17 +51,22 @@ else:
 similarity_score = list(enumerate(similarity[anime_index]))
 sorted_similarity_score = sorted(similarity_score, key=lambda x: x[1], reverse=True)
 
-i = 1
-for anime in sorted_similarity_score:
-    index = anime[0]
-    if jap:
-        anime_from_index = dataset[dataset.AnimeIndex == index]['JapaneseName'].values
-    else:
-        anime_from_index = dataset[dataset.AnimeIndex == index]['EnglishName'].values
+def get_anime_name(index, language):
+    result = dataset[dataset.AnimeIndex == index][language].values
+    return result[0] if result.size > 0 else None
 
-    if len(anime_from_index) > 0:
-        print(i, " ", anime_from_index[0])
+i = 1
+for anime in sorted_similarity_score[1: 10]:
+    index = anime[0]
+    if not jap:
+        anime_from_index = get_anime_name(index, 'EnglishName')
+        if anime_from_index == 'UNKNOWN' or anime_from_index is None:
+            anime_from_index = get_anime_name(index, 'JapaneseName')
+    else:
+        anime_from_index = get_anime_name(index, 'JapaneseName')
+
+    if anime_from_index:  # Check if not None or empty
+        print(i, " ", anime_from_index)
         i += 1
-    
-    if i > 10:
-        break
+    else:
+        print('anime not found')
